6b3770973851c25930f4ba27556d726a012805df
Winfield H <winterfreshness@gmail.com>
2018-12-21 17:24:41 -0500

    Modified connection.js / schema.sql for Heroku

--- db/schema.sql
+++ db/schema.sql
@@ -10,5 +10,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(100) NOT NULL,
-    devoured BOOLEAN
+    devoured BOOLEAN DEFAULT false,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
