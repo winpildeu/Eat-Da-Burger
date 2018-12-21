dc9630139e8bc93282b8fd0f079db1cbf3637e07
Winfield H <winterfreshness@gmail.com>
2018-12-21 18:33:24 -0500

    main.handlebars/ orm.js/ burgers_controller.js/ models/burger.js made

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,29 @@
-const orm = require('../config/orm');
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
 
-// call the ORM functions using the burger input
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
+};
 
-// export something
-// module.exports = ;
+// Export the database functions for the controller (burgers_controller.js).
+module.exports = burger;
--- server.js
+++ server.js
@@ -1,3 +1,32 @@
-// import the express module
-const express = require('express');
-const app = express();
+// Import the express module
+var express = require('express');
+
+// Setup the port for the server
+var PORT = process.env.PORT || 8080;
+
+// Start up the express app
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+// Set the express app to use Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
