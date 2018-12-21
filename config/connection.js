5e6adb044a2e1f6a8cbf294ad7650d227ec66b98
Winfield H <winterfreshness@gmail.com>
2018-12-21 17:07:02 -0500

    Changed connection.js for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -2,21 +2,27 @@
 const mysql = require('mysql');
 
 // setup the connection settings
-const connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: 'localhost',
     port: 8889,
     user: 'root',
     password: 'root',
     database: 'burgers'
-});
+  });
+}
 
 // make the connection and log where it connects
 connection.connect((err) => {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
 // export the connection to be used by the ORM
