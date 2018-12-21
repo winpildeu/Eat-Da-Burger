db0f13129ad88fda7568c25e01b44ad2a07e7233
Winfield H <winterfreshness@gmail.com>
2018-12-21 16:46:32 -0500

    Before heroku deployment

---
+++ config/orm.js
@@ -0,0 +1,10 @@
+const connection = require('./connection');
+
+const orm = {
+    selectAll : function() {},
+    insertOne: function() {},
+    updateOne: function() {}
+};
+
+// export the orm object to be used in the app
+module.exports = orm;
